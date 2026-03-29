5d37697fbe534af933a14e44b1c42e65e2b26ba9
shivamlalakiya <shivamlalakiya151299@gmail.com>
2026-03-29 11:39:36 -0500

    Fix GA4 implementation with correct Measurement ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense } from "react";
 import "./App.css";
 import SuspenseFallback from "./components/SuspenseFallback";
-import ReactGA from "react-ga4";
 
 const CharacterModel = lazy(() => import("./components/Character"));
 const MainContainer = lazy(() => import("./components/MainContainer"));
 import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
-  useEffect(() => {
-    const initAnalytics = () => {
-      ReactGA.initialize("G-ZC359GW4XVID");
-      ReactGA.send({
-        hitType: "pageview",
-        page: window.location.pathname + window.location.search,
-        title: document.title,
-      });
-    };
-
-    // Defer GA init so it doesn't compete with Three.js startup.
-    const ric = (window as unknown as { requestIdleCallback?: Function })
-      .requestIdleCallback;
-    if (ric) {
-      ric(initAnalytics, { timeout: 2000 });
-    } else {
-      setTimeout(initAnalytics, 1);
-    }
-  }, []);
-
   return (
     <>
       <LoadingProvider>
